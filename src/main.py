35e8decff88cfbb9b5d808524705f19890fdd94a
vanila <idegtiareov@mail.ru>
2023-05-17 04:21:40 +0300

    bug fixes

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def main():
     data = get_last_values(data, count_last_values=COUNT_LAST_VALUES)
     data = get_formatted_data(data)
     for row in data:
-         print(row)
-# , end='\n\n'
+         print(row, end='\n\n')
+
 if __name__ == '__main__':
     main()
